f7fd347d0069dafe284f9d9b180e758ec6c5ec3d
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2024-03-27 16:40:40 +0200

    filter styles end

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,33 +2,54 @@ import Select from "react-select";
 
 import { getAllValues } from "../../helpers/getAllValues";
 
-import { SelectWrapper, customStyles } from "./Filter.styled";
+import { Label, SelectWrapper, customStyles } from "./Filter.styled";
 
 const Filter = ({ teachers }) => {
   const levels = getAllValues(teachers, "levels");
   const languages = getAllValues(teachers, "languages");
   const prices = getAllValues(teachers, "price_per_hour");
 
+  const handleLanguageChange = (option) => {
+    console.log("option", option);
+  };
+
+  const handleLevelChange = (option) => {
+    console.log("option", option);
+  };
+
+  const handlePriceChange = (option) => {
+    console.log("option", option);
+  };
+
   return (
     <SelectWrapper>
-      <Select
-        isClearable
-        placeholder={"Languages..."}
-        options={languages.map((level) => ({ value: level, label: level }))}
-        styles={customStyles}
-      />
-      <Select
-        isClearable
-        placeholder={"Levels..."}
-        options={levels.map((level) => ({ value: level, label: level }))}
-        styles={customStyles}
-      />
-      <Select
-        isClearable
-        placeholder={"Prices per hour... "}
-        options={prices.map((level) => ({ value: level, label: `${level} $` }))}
-        styles={customStyles}
-      />
+      <div>
+        <Label>Languages</Label>
+        <Select
+          onChange={handleLanguageChange}
+          isClearable
+          options={languages.map((level) => ({ value: level, label: level }))}
+          styles={customStyles(240)}
+        />
+      </div>
+      <div>
+        <Label>Level of knowledge</Label>
+        <Select
+          onChange={handleLevelChange}
+          isClearable
+          options={levels.map((level) => ({ value: level, label: level }))}
+          styles={customStyles(270)}
+        />
+      </div>
+      <div>
+        <Label>Price</Label>
+        <Select
+          onChange={handlePriceChange}
+          isClearable
+          options={prices.map((level) => ({ value: level, label: `${level} $` }))}
+          styles={customStyles(140)}
+        />
+      </div>
     </SelectWrapper>
   );
 };
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -7,22 +7,63 @@ export const SelectWrapper = styled.div`
   margin-bottom: 2rem;
 `;
 
-export const customStyles = {
+export const Label = styled.p`
+  font-size: 0.875rem;
+  line-height: 1.29;
+
+  color: var(--color-grey);
+
+  margin-bottom: 0.5rem;
+`;
+
+export const customStyles = (width) => ({
   singleValue: (styles) => ({
     ...styles,
     color: "var(--color-black)",
     margin: 0,
   }),
-  control: (styles, { isHover }) => ({
+
+  control: (styles) => ({
     ...styles,
     backgroundColor: "var(--color-white)",
-    borderColor: "var(--color-black-20)",
+    border: "2px solid transparent",
     borderRadius: 14,
     cursor: "pointer",
-    minWidth: 260,
+    width,
     boxShadow: "none",
     transition: "var(--transition)",
     ":hover": { borderColor: "var(--color-primary)" },
+  }),
+
+  valueContainer: (styles) => ({
+    ...styles,
+    padding: "0.5rem 0.875rem",
+  }),
+
+  menu: (styles) => ({
+    ...styles,
+    padding: "14px 18px",
+    borderRadius: "12px",
+    border: "none",
+  }),
+
+  menuList: (styles) => ({
+    ...styles,
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.5rem",
+    boxSizing: "content-box",
+    "&::-webkit-scrollbar": {
+      width: "0.25rem",
+      position: "relative",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      borderRadius: "12px",
+      background: "var(--color-secondary)",
+    },
+    "&::-webkit-scrollbar-track": {
+      background: "transparent",
+    },
   }),
 
   option: (styles, { isSelected, isFocused }) => ({
@@ -33,40 +74,27 @@ export const customStyles = {
       ? "var(--color-black-70)"
       : "var(--color-black-20)",
     backgroundColor: "var(--color-white)",
-    borderRadius: "12px",
+    padding: 0,
   }),
-};
 
-// CONTROL
-// &:hover: borderColor: "#2684FF"
-// alignItems: "center"
-// backgroundColor: "hsl(0, 0%, 100%)"
-// borderColor: "#2684FF"
-// borderRadius: 4
-// borderStyle: "solid"
-// borderWidth: 1
-// boxShadow: "0 0 0 1px #2684FF"
-// boxSizing: "border-box"
-// cursor: "default"
-// display: "flex"
-// flexWrap: "wrap"
-// justifyContent: "space-between"
-// label: "control"
-// minHeight: 38
-// outline: "0 !important"
-// position: "relative"
-// transition: "all 100ms"
+  placeholder: (styles) => ({
+    ...styles,
+    color: "var(--color-black)",
+  }),
 
-//OPTION
-// :active: backgroundColor: "#B2D4FF"
-// WebkitTapHighlightColor: "rgba(0, 0, 0, 0)"
-// backgroundColor: "transparent"
-// boxSizing: "border-box"
-// color: "inherit"
-// cursor: "default"
-// display: "block"
-// fontSize: "inherit"
-// label: "option"
-// padding: "8px 12px"
-// userSelect: "none"
-// width: "100%"
+  indicatorSeparator: () => ({
+    display: "none",
+  }),
+
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    color: "var(--color-black)",
+    ":hover": { color: "var(--color-primary)" },
+  }),
+
+  clearIndicator: (styles) => ({
+    ...styles,
+    color: "var(--color-black)",
+    ":hover": { color: "var(--color-primary)" },
+  }),
+});
